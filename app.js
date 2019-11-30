e8cfbfff8f5ea6c90b90f8ab46f837d4e27dd780
Ing Jhon barranco <jhonbarranco@iMac-de-Ing.local>
2019-11-30 07:57:10 -0500

    sss

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("iniciando ts");
+var product = {
+    id: 'abc1',
+    price: 12.99,
+    tags: ['great-offer', 'hot-and-new'],
+    details: {
+        title: 'Red Carpet',
+        description: 'A great carpet - almost brand-new!'
+    }
+};
+console.log(product);
+console.log(product.tags[0]);
--- app.ts
+++ app.ts
@@ -1,1 +1,22 @@
-console.log("iniciando ts");
+const product: {
+    id: string;
+    price: number;
+    tags: string[],
+    details: {
+              title: string;
+              description: string;
+            } 
+    } = {
+        id: 'abc1',
+        price: 12.99,
+        tags: ['great-offer', 'hot-and-new'],
+        details: {
+        title: 'Red Carpet',
+        description: 'A great carpet - almost brand-new!'
+        }
+  }
+
+
+console.log(product);
+
+console.log(product.tags[0]);
